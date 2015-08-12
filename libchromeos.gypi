msft-mirror-aosp/platform.external.libbrillo

commit 14dd66b0a440eb1a97e41f2086051bac747fd50a

libchromeos-minijail: Fix the include directory.
minijail.h was updated to include libminijail.h instead of
chromeos/minijail.h.
This change must be reflected in the gyp file.

Change-Id: I8ae80e8878b8d09a90768a8f2b10c8be7eb3f2eb

libchromeos.gypi
@@ -192,6 +192,7 @@
       'sources': [
         'chromeos/minijail/minijail.cc',
       ],
+      'includes': ['<(sysroot)/usr/include/chromeos'],
     },
     {
       'target_name': 'libchromeos-ui-<(libbase_ver)',

commit 5d7fc29f9f5a9c9858f3df2395eed31abe72c5a7

minijail: Fix import path.
Fix the bug introduced by 14dd66b0a440eb1a97e41f2086051bac747fd50a.
'includes' includes a gyp file. include_dirs is the list of directories
to include.

TEST=`emerge-gizmo libchromeos` works with this change.

Change-Id: I2b606713e53b4775a78bdcb6f66fe15c4b94ca45

libchromeos.gypi
@@ -192,7 +192,7 @@
       'sources': [
         'chromeos/minijail/minijail.cc',
       ],
-      'includes': ['<(sysroot)/usr/include/chromeos'],
+      'include_dirs': ['<(sysroot)/usr/include/chromeos'],
     },
     {
       'target_name': 'libchromeos-ui-<(libbase_ver)',
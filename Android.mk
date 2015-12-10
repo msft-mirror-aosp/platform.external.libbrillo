24466ace63e0f685dbf9bdd71c21d019f8680c59
Alex Deymo <deymo@google.com>
2015-12-09 17:13:41 -0800

    Build libbrillo-http and libbrillo-stream for the host.
    To be aligned with the new build system, the host and target modules for
    libbrillo-* libraries should be the same. This patch creates two new
    modules for building libbrillo-stream and libbrillo-http for the host
    and renames libbrillo-host to libbrillo.
    
    Bug: 24619596
    TEST=mma on linux and mac
    
    Change-Id: I8a17b14e2ac598e7f7e5c586ead5a9d19d59c778
    (cherry picked from commit dc87856e399654209fcaab6732669516009d802c)

--- Android.mk
+++ Android.mk
@@ -301,11 +301,43 @@ include $(BUILD_STATIC_LIBRARY)
 # ========================================================
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := $(libbrillo_cpp_extension)
-LOCAL_MODULE := libbrillo-host
+LOCAL_MODULE := libbrillo
 LOCAL_SRC_FILES := $(libbrillo_core_sources)
 LOCAL_C_INCLUDES := $(libbrillo_includes)
-LOCAL_SHARED_LIBRARIES := libchrome-host
+LOCAL_SHARED_LIBRARIES := $(libbrillo_shared_libraries)
 LOCAL_STATIC_LIBRARIES := libmodpb64-host
+LOCAL_CFLAGS := $(libbrillo_CFLAGS)
+LOCAL_CPPFLAGS := $(libbrillo_CPPFLAGS)
+LOCAL_RTTI_FLAG := -frtti
+LOCAL_CLANG := true
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+include $(BUILD_HOST_SHARED_LIBRARY)
+
+# Shared stream library for target
+# ========================================================
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := $(libbrillo_cpp_extension)
+LOCAL_MODULE := libbrillo-stream
+LOCAL_SRC_FILES := $(libbrillo_stream_sources)
+LOCAL_C_INCLUDES := $(libbrillo_includes)
+LOCAL_SHARED_LIBRARIES := $(libbrillo_shared_libraries) libbrillo \
+    libcrypto-host libssl-host
+LOCAL_CFLAGS := $(libbrillo_CFLAGS)
+LOCAL_CPPFLAGS := $(libbrillo_CPPFLAGS)
+LOCAL_RTTI_FLAG := -frtti
+LOCAL_CLANG := true
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+include $(BUILD_HOST_SHARED_LIBRARY)
+
+# Shared http library for target
+# ========================================================
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := $(libbrillo_cpp_extension)
+LOCAL_MODULE := libbrillo-http
+LOCAL_SRC_FILES := $(libbrillo_http_sources)
+LOCAL_C_INCLUDES := $(libbrillo_includes)
+LOCAL_SHARED_LIBRARIES := $(libbrillo_shared_libraries) libbrillo \
+    libbrillo-stream libcurl-host
 LOCAL_CFLAGS := $(libbrillo_CFLAGS)
 LOCAL_CPPFLAGS := $(libbrillo_CPPFLAGS)
 LOCAL_RTTI_FLAG := -frtti
